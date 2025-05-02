9c6120bf800dabb62b6a3463ad5d35dd838d7e0a
mohamad nurdiansyah <mohamad.nurdiansyah@mediawave.co.id>
2025-05-02 14:49:44 +0700

    db configuration

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -6,12 +6,19 @@ use CodeIgniter\Database\Config;
 
 class Database extends Config
 {
+    /**
+     * The default database connection.
+     */
     public $defaultGroup = 'default';
+
+    /**
+     * The default database connection.
+     */
     public $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
         'username' => 'root',
-        'password' => 'password',
+        'password' => '',
         'database' => 'ci_api',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
@@ -27,8 +34,18 @@ class Database extends Config
         'port'     => 3306,
     ];
 
+    /**
+     * Constructor.
+     */
     public function __construct()
     {
         parent::__construct();
+
+        // Set database credentials from environment variables
+        $this->default['hostname'] = env('database.default.hostname') ?: $this->default['hostname'];
+        $this->default['username'] = env('database.default.username') ?: $this->default['username'];
+        $this->default['password'] = env('database.default.password') ?: $this->default['password'];
+        $this->default['database'] = env('database.default.database') ?: $this->default['database'];
+        $this->default['port'] = (int)(env('database.default.port') ?: $this->default['port']);
     }
 }
